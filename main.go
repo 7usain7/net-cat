c0fde3888eb12452c5f2695fcd4769188a88be91
7usain7 <h.jameel2004@gmail.com>
2025-09-02 05:06:03 +0300

    initialize data variables and main file

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net-cat/funcs"
+	"os"
+)
+
+func main() {
+	if len(os.Args) > 2 {
+		fmt.Println("[USAGE]: go run main.go $port")
+		return
+	} else if len(os.Args) == 2 {
+		funcs.DEFULT_PORT = os.Args[1]
+	}
+
+	funcs.ServerHandler(funcs.DEFULT_PORT)
+}
