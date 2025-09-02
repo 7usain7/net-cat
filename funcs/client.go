d67fe68f836a3460571bb0acd1ff4d4e76a10e69
7usain7 <h.jameel2004@gmail.com>
2025-09-02 05:18:14 +0300

    the server handler, start and listen for new connections

---
+++ funcs/client.go
@@ -0,0 +1,9 @@
+package funcs
+
+import (
+	"net"
+)
+
+func HandleClient(conn net.Conn) { // net.Conn = the network connection between the server and the client
+
+}
--- funcs/server.go
+++ funcs/server.go
@@ -1,5 +1,35 @@
 package funcs
 
+import (
+	"fmt"
+	"net"
+)
+
 func ServerHandler(port string) {
+
+	// Accepts incoming TCP connections
+	server, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+		return
+	}
+	defer server.Close()
+
+	fmt.Println("Server is listening on port", port)
+
+	go Broadcast() // Start listening to new messages
+
+	for {
+		conn, err := server.Accept() // Wait for a new clients
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+			continue
+		}
+
+		go HandleClient(conn) // Start a new goroutine for this client
+	}
+}
+
+func Broadcast() {
 
 }
