c1e0c5eebfd47287f64e756b2c289ed7956ed40b
7usain7 <h.jameel2004@gmail.com>
2025-09-15 17:50:03 +0300

    last fix

--- main.go
+++ main.go
@@ -4,13 +4,20 @@ import (
 	"fmt"
 	"net-cat/funcs"
 	"os"
+	"strconv"
 )
 
 func main() {
+	args := os.Args
 	if len(os.Args) > 2 {
 		fmt.Println("[USAGE]: ./TCPChat $port")
 		return
 	} else if len(os.Args) == 2 {
+		if _, err := strconv.Atoi(args[1]); err != nil {
+			fmt.Println("[USAGE]: ./TCPChat $port")
+			return
+		}
+
 		funcs.DEFULT_PORT = os.Args[1]
 	}
 
